a694acbb7a369b3e9cfff89266a1e57d030f5b1f
thadeAUx <tja0010@auburn.edu>
2025-01-14 18:35:32 -0600

    Initial Commit

---
+++ au hello world/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  au hello world
+//
+//  Created by Thad Aldridge on 1/14/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
